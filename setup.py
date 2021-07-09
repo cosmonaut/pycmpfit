2be4c2abf3402cf8b6a1c9ed6d4077bd9124d17d
nell <nell@lasp.colorado.edu>
2021-07-09 14:13:10 -0600

    Update setup.py to allow distribution on pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+#from setuptools import find_packages, setup
+from setuptools import find_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from numpy.distutils.misc_util import get_numpy_include_dirs
@@ -11,15 +13,21 @@ ext_modules = Extension("pycmpfit", ["pycmpfit.pyx", "cmpfit-1.4/mpfit.c"],
 )
 
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name = 'pycmpfit',
-    version = '0.1.0',
+    version = '0.2.0',
+    packages=find_packages(),
     description = '',
+    long_description = long_description,
+    #long_description_content_type = 'text/markdown',
     author = 'Nicholas Nell',
     author_email = 'nicholas.nell@colorado.edu',
     url = 'https://github.com/cosmonaut/pycmpfit',
-    packages = 'pycmpfit',
-    provides = 'pycmpfit',
+    #packages = ['pycmpfit'],
+    provides = ['pycmpfit'],
     requires = ['numpy'],
     keywords = ['Scientific/Engineering'],
     cmdclass = {'build_ext': build_ext},
